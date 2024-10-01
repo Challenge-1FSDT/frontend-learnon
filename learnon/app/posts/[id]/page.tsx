bfebbc5982f235e5b964514eb03b0263df5cd9fc
Anelise Estevam <aestevam.dev@gmail.com>
2024-10-01 09:27:52 -0300

    updating post page with date and author

--- learnon/app/posts/[id]/page.tsx
+++ learnon/app/posts/[id]/page.tsx
@@ -2,17 +2,30 @@ import Navbar from '@/app/components/Navbar';
 import { getPost } from '../../lib/posts';
 import FooterPost from '@/app/components/Footer-post';
 
-
-// eslint-disable-next-line @next/next/no-async-client-component
 export default async function PostPage({ params }: { params: { id: string } }) {
   const post = await getPost(params.id);
+  
+  const date = new Date(post.createdAt).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  });
+
 
   return (
     <div>
       <Navbar />
-      <div className='flex flex-col items-center h-auto'>
-        <h1 className='text-black p-6 text-4xl font-bold'>{post.title}</h1>
-        <p className='text-gray-700 px-16 text-xl text-justify'>{post.content}</p>
+      <div className='mx-auto p-16'>
+        <div className='flex flex-col h-auto'>
+          <h1 className='text-black text-4xl font-bold'>{post.title}</h1>
+          <div className='flex flex-col h-auto mt-4'>
+            <p className='text-gray-700 font-semibold'>Author: {post.author}</p>
+          </div>
+          <div className='flex flex-col h-auto'>
+            <p className='text-gray-700 font-semibold'>Publicado em: {date}</p>
+          </div>
+          <p className='text-gray-700 mt-4 text-justify'>{post.content}</p>
+        </div>
       </div>
       <FooterPost />
     </div>
